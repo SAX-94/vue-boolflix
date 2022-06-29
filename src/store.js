0d17553fc788e0e71d1c0a7fae6a8e4a59ec58d1
SAX_94 <matteosacchi94@gmail.com>
2022-06-29 19:35:37 +0200

    added Axios callback for tv series

--- src/store.js
+++ src/store.js
@@ -18,4 +18,16 @@ export function searchMovies(userSearch) {
         .then((resp) => {
             state.moviesList = resp.data.results;
         });
+
+    axios
+        .get("https://api.themoviedb.org/3/search/tv", {
+            params: {
+                api_key: "7147a9e71923a77d5c6a5bca282eaa18",
+                query: userSearch,
+                language: "it-IT",
+            },
+        })
+        .then((resp) => {
+            state.seriesList = resp.data.results;
+        });
 }
