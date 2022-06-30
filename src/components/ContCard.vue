31e2a834908c3b696e6fc88af3e7c73f4ddcb669
SAX_94 <matteosacchi94@gmail.com>
2022-06-30 09:30:36 +0200

    card created for render

---
+++ src/components/ContCard.vue
@@ -0,0 +1,115 @@
+<template>
+    <div>
+<!-------  MOVIES  ------->
+        <div>
+            <ul class="list-unstyled text-center" v-for="(movie, i) in moviesList" :key="i">
+                <li>
+                    <div class="container text-start">
+                        <span class=" text-danger fs-3">Movies:</span>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <img :src="boolflixPoster + [movie.poster_path]" :alt="movie.title"> <!-- Movie poster-->
+                    </div>
+                </li>
+                <li class="my-2">
+                    <span class="text-primary">Title</span> :
+                    <span class="text-success">{{ movie.title }}</span> <!-- MOvie title -->
+                </li>
+                <li class="my-2">
+                    <span class="text-primary">Original Title</span> :
+                    <span class="text-danger">{{ movie.original_title }}</span> <!-- Original title -->
+                </li>
+                <li class="my-2">
+                    <span class="text-primary">Languages </span> :
+                    <span class="text-info">{{ movie.original_language }}</span>
+                    <lang-flag :iso="movie.original_language"></lang-flag> <!-- lOriginal laguage -->
+                </li>
+                <li class="my-2 border-bottom">
+                    <span class="text-primary">Vote </span> :
+                    <span v-html="getStars(movie)"></span> <!-- Average vote -->
+                </li>
+            </ul>
+        </div>
+
+<!-------  TV SERIES  ------->
+        <div>
+            <ul class="list-unstyled text-center" v-for="(serie, i) in seriesList" :key="i">
+                <li>
+                    <div class="container">
+                        <img :src="boolflixPoster + [serie.poster_path]" :alt="serie.name">
+                    </div>
+                </li>
+                <li>
+                    <div class="container text-start">
+                        <span class="text-secondary fs-3">TV series:</span>
+                    </div>
+                </li>
+                <li class="my-2 ">
+                    <span class="text-warning">Title</span> <span class="text-success">{{ serie.name }} </span>
+                    <!-- title of series -->
+                </li>
+                <li class="my-2 ">
+                    <span class="text-warning">Original Title</span> <span
+                        class="text-danger">{{ serie.original_name }}</span> <!-- original title of series -->
+                </li>
+                <li class="my-2 ">
+                    <span class="text-warning">Languages</span> <span
+                        class="text-info">{{ serie.original_language }}</span>
+                    <lang-flag :iso="serie.original_language"></lang-flag> <!-- languages of series -->
+                </li>
+                <li class="my-2 border-bottom">
+                    <span class="text-warning">Vote</span>
+                    <span v-html="getStars(serie)"></span> <!-- vote_average of series -->
+                </li>
+            </ul>
+
+        </div>
+
+
+    </div>
+</template>
+
+
+<script>
+import { state } from "../store";
+export default {
+    data() {
+        return {
+            voteArray: [1, 2, 3, 4, 5]
+        }
+    },
+    computed: {
+        moviesList() {
+            return state.moviesList;
+        },
+        seriesList() {
+            return state.seriesList;
+        },
+        boolflixPoster() {
+            return "https://image.tmdb.org/t/p/" + "w342/"
+        },
+
+    },
+    methods: {
+        // function for my stars under img
+        getStars(vote) {
+            const votes = Math.ceil(vote.vote_average / 2);
+            const fullStar = [];
+            const emptyStar = [];
+            for (let i = 0; i < votes; i++) {
+                fullStar.push("<i class=\"fa fa-solid fa-star\"></i>");
+            }
+            for (let i = 0; i < (5 - votes); i++) {
+                emptyStar.push("<i class=\"fa fa-regular fa-star\"></i>")
+            }
+            const fullVotes = fullStar.join("") + emptyStar.join("");
+            return fullVotes
+        },
+    },
+}
+</script>
+
+<style>
+</style>
